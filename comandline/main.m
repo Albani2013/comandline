2f44ab1f85b432f9f5fbecd69a9d9347af638c2d
Albani2013 <albani_com@hotmail.com>
2013-04-11 18:53:45 +0200

    Initial Commit

---
+++ comandline/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  comandline
+//
+//  Created by ICK Training on 4/11/13.
+//  Copyright (c) 2013 ICK Training. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
